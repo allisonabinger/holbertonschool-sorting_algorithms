5c7cece697dccfc9e3b41c5431cd2c61cf827eef
allisonabinger <allisonabinger@gmail.com>
2023-08-21 09:52:17 -0700

    gave swap its own file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,4 @@
-#include "sort.h"
-/**
- * _swap - swaps the placement of two integers using temporary variable
- * @xp: pointer to first integer
- * @yp: pointer to second integer
- */
- void _swap(int *xp, int *yp)
- {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
- }
-
+#include "sort.h"./
 /**
  * bubble_sort - sorts an array of integers in ascending order
  * @array: array of integers
