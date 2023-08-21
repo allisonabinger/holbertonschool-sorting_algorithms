e1039377630f923c6223abde355fc48a1d0376d4
allisonabinger <allisonabinger@gmail.com>
2023-08-21 16:14:41 -0700

    prints once

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,7 +26,6 @@ int partition(int* array, int low, int high, size_t size)
 	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
-	print_array(array, size);
 
 	return (i + 1);
 }
