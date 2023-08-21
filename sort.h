90149bcb51c16aea5c4bcf88bf18f9e4c7f3082e
allisonabinger <allisonabinger@gmail.com>
2023-08-21 09:33:49 -0700

    moved _swap declaration

--- sort.h
+++ sort.h
@@ -26,12 +26,12 @@ typedef struct listint_s
 
 /*Prototypes*/
 
-void _swap(int *xp, int *yp);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void _swap(int *xp, int *yp);
 
 #endif
