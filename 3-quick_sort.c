90b303ef75a15bf5ebaacc7423b553f097a165d1
allisonabinger <allisonabinger@gmail.com>
2023-08-21 15:17:31 -0700

    removed call to swap in 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,17 +12,22 @@ int *partition(int* array, int low, int high)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int j;
+	int j, temp;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 		}
 	}
-	swap(&array[i + 1], &array[high]);
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	
 	return (i + 1);
 }
 
